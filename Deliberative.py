7b13be103a02625062b09fffc4a5035661ef3daf
Miguel Simões <miguel_simoes@outlook.com>
2019-05-22 14:21:19 +0100

    Deliberative Agents -FV

--- Deliberative.py
+++ Deliberative.py
@@ -3,16 +3,17 @@ import random
 import math
 
 SIZE= 800
-SPRITE_SCALING_CAR = 0.02
+SPRITE_SCALING_CAR = 0.03
 SPRITE_SCALING_WALL = 0.1
 
 C = SIZE/2
 GREY = (128,128,128)
 WHITE = (255,255,255)
 ROAD_HEIGHT = 200
-MOVEMENT_SPEED = 25
+MOVEMENT_SPEED = 15
 AREA = []
 QUEUE = []
+#NUMBER_OF_CARS_IN_INTERSECTION=0
 
 class Car(arcade.Sprite):
 
@@ -24,67 +25,124 @@ class Car(arcade.Sprite):
         self.message = None
         self.collision_point = {}
         self.entrei = False
+        self.number_of_paused =0
+        self.number_of_blocks=0
+        self.car_who_blocked_me=None
         
         super().__init__(image, scale)
 
     def update(self):
+        #global NUMBER_OF_CARS_IN_INTERSECTION
+
         distance_to_all_cars = [arcade.get_distance_between_sprites(self,x) for x in self.other_cars_list]
         #Quando passaste o ponto de colisão
-        if(self.entrei==True and (self.center_x,self.center_y) in self.collision_point.keys()):
-            print("vou libertar os gajos")
-            carros_list = self.collision_point[(self.center_x,self.center_y)]
-            for carrinho in carros_list:
-                carrinho.message=None
+        #if(self.entrei==True and (self.center_x,self.center_y) in self.collision_point.keys()):
+        if(self.entrei==True and (self.center_x,self.center_y) not in AREA):
+            #NUMBER_OF_CARS_IN_INTERSECTION -=1
+            for pos in self.collision_point:
+                for carrinho in self.collision_point[pos]:    
+                    carrinho.message=None
+
             self.entrei = False
-            self.collision_point.pop((self.center_x,self.center_y))
+          #  print("iff 1")
+
 
         #kill
         if(int(self.center_x) == int(self.goal[0]) and int(self.center_y) == int(self.goal[1])):
+            #print("iff 2")
             self.kill()
-
+     
         #Slow down
         elif((self.array_pos + MOVEMENT_SPEED) > len(self.path)-1):
             self.center_x = self.path[self.array_pos][0]
             self.center_y = self.path[self.array_pos][1]
             self.array_pos +=1 
+           # print("ifff 3")
 
-        #SE ALGUÉM JÁ deu set na mensagem
-        elif(self.message != None):
-            self.center_x = self.path[self.array_pos][0]
-            self.center_y = self.path[self.array_pos][1]
-            self.array_pos += 0
 
         #MOVE AFTER STOP
-        elif(len(list(filter(lambda y : y == True,[(x.center_x,x.center_y) in self.path[self.array_pos:self.array_pos+100] for x in self.other_cars_list])))==0):
+        elif(len(list(filter(lambda y : y == True,[(x.center_x, x.center_y) in self.path[self.array_pos:self.array_pos+100] for x in self.other_cars_list])))==0):
+            #print("ifff 4")
+            #if(NUMBER_OF_CARS_IN_INTERSECTION<3):
             self.center_x = self.path[self.array_pos][0]
             self.center_y = self.path[self.array_pos][1]
             self.array_pos += MOVEMENT_SPEED
 
         #caso da distancia de travagem
-        elif(len(list(filter(lambda x: x < 70 and x > 0, distance_to_all_cars)))>0):
+        elif(len(list(filter(lambda x: x < 20 and x>0, distance_to_all_cars)))>0):
+            #print("ifff 5")
             self.center_x = self.path[self.array_pos][0]
             self.center_y = self.path[self.array_pos][1]
             self.array_pos += 0
+
+
+        #SE ALGUÉM JÁ deu set na mensagem
+        elif(self.message != None):
+            #print("ifff 6")
+            #print("car who blocked me : ", (self.car_who_blocked_me.center_x, self.car_who_blocked_me.center_y))
+            #print("car who blocked me (MESSAGE): ", self.car_who_blocked_me.message)
+            #print("# blocks: ",self.number_of_blocks)
+            self.center_x = self.path[self.array_pos][0]
+            self.center_y = self.path[self.array_pos][1]
+            self.array_pos += 0
+            self.number_of_blocks +=1
+            
+            # desbloqueia-se a ele proprio quando o carro que o bloqueou ja nao esta dentro da area de intersecao
+            if((self.car_who_blocked_me.center_x, self.car_who_blocked_me.center_y) not in AREA and self.number_of_blocks>5): 
+                self.message=None
+
+            # o carro que o bloqueou tambem esta bloqueado
+            elif(self.car_who_blocked_me.message != None): 
+                self.message=None
+
+            # o carro que o bloqueou nao esta bloqueado, mas por alguma razao tambem nao anda, por isso obrigar o outro gajo a andar
+            elif(self.car_who_blocked_me.message == None): # obrigar o gajo que o bloqueou a andar
+                self.car_who_blocked_me.center_x = self.car_who_blocked_me.path[self.car_who_blocked_me.array_pos][0]
+                self.car_who_blocked_me.center_y = self.car_who_blocked_me.path[self.car_who_blocked_me.array_pos][1]
+                self.car_who_blocked_me.array_pos+=MOVEMENT_SPEED
+
+       
+
         #IN AREA
         elif((self.center_x,self.center_y) in AREA and self.entrei==False):
+            #print("# cars: ",NUMBER_OF_CARS_IN_INTERSECTION)
+
+            '''
+            if(NUMBER_OF_CARS_IN_INTERSECTION>3):
+                self.center_x = self.path[self.array_pos][0]
+                self.center_y = self.path[self.array_pos][1]
+                self.array_pos += 0
+            else:
+            '''
+            #NUMBER_OF_CARS_IN_INTERSECTION +=1
             cars_inside = [x for x in self.other_cars_list if (x.center_x,x.center_y) in AREA]
             counter=0
             for car in cars_inside:
+                #if (car.message == None): # so bloqueia carros que ainda nao estao bloqueados
+
                 common = [value for value in self.path[self.array_pos:self.array_pos+300] if value in car.path[car.array_pos:car.array_pos+300]]
 
                 if(len(common)>0):
-                    if( len(self.path[self.array_pos:]) < len(car.path[car.array_pos:])): # se eu estiver mais perto do destino, bloqueio o outro 
+                    mine_n_iterations = abs(common[0][0] - self.center_x) + abs(common[0][1] - self.center_y)
+                    other_n_iterations = abs(common[0][0] - car.center_x) + abs(common[0][1] - car.center_y)
+
+                    if(mine_n_iterations < other_n_iterations): # a mesma distancia do ponto de colisao
+                    #if( len(self.path[self.array_pos:]) < len(car.path[car.array_pos:])): # so bloqueia aqueles que estao mais longe do destino do que ele proprio
+                        #print("mine: ",mine_n_iterations)
+                        #print("other: ",other_n_iterations)
+
                         counter+=1
                         if (common[0] in self.collision_point.keys()):
                             cars_list=self.collision_point[common[0]]
                             cars_list.append(car)
                             self.collision_point[common[0]] = cars_list
                         else:
-                            self.collision_point[common[0]] = [car] 
+                            self.collision_point[common[0]] = [car]
                         car.message = 0
-            
-                        print("Eu ",(self.center_x,self.center_y ),"vou bloquear ",counter, " gajos")
-
+                        car.car_who_blocked_me = self
+    
+           # if(counter!=0):
+                #print("bloacked : ",counter)            
             self.entrei = True
             self.center_x = self.path[self.array_pos][0]
             self.center_y = self.path[self.array_pos][1]
@@ -92,6 +150,7 @@ class Car(arcade.Sprite):
 
         #caso de circulacao normal
         else:
+            #print("iff 7")
             self.center_x = self.path[self.array_pos][0]
             self.center_y = self.path[self.array_pos][1]
             self.array_pos += MOVEMENT_SPEED
@@ -153,6 +212,7 @@ class MyGame(arcade.Window):
             arcade.close_window()
         for car in self.cars_list:
             car.other_cars_list = list((filter(lambda x : x != car,self.cars_list)))
+
         self.cars_list.update()
 
 
@@ -170,8 +230,14 @@ class MyGame(arcade.Window):
 
             path = self.create_paths(depart_coords, goal_coords)
 
-            self.car_sprite = Car("car.png", SPRITE_SCALING_CAR, path, goal_coords)
-
+            if (depart == 1):
+                self.car_sprite = Car("car.png", SPRITE_SCALING_CAR, path, goal_coords)
+            elif(depart ==2):
+                self.car_sprite = Car("car1.png", SPRITE_SCALING_CAR, path, goal_coords)
+            elif(depart == 3):
+                self.car_sprite = Car("car2.png", SPRITE_SCALING_CAR, path, goal_coords)
+            else:
+                self.car_sprite = Car("car3.png", SPRITE_SCALING_CAR, path, goal_coords)
             self.car_sprite.center_x = self.departCoords[depart][0]    
             self.car_sprite.center_y = self.departCoords[depart][1]
 
